dc4deac63e10f26c7e0dfac6ecf5f1f48623d1b6
Behnam Amiri <amiri.behnam4@gmail.com>
2016-02-22 13:14:04 -0700

    New machine[axof:49]

--- Calculator.Tests/CalculatorOperation.Test.cs
+++ Calculator.Tests/CalculatorOperation.Test.cs
@@ -7,7 +7,7 @@ namespace Calculator.Tests
     public class CalculatorOperationTest
     {
         /// <summary>
-        /// 
+        /// asdsdsadsd
         /// </summary>
         // first tester method
         [TestMethod]
@@ -22,7 +22,7 @@ namespace Calculator.Tests
 
 
         /// <summary>
-        /// 
+        /// sdsadd
         /// </summary>
         // second tester method
         [TestMethod]
